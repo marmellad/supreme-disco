1c2f76d83d1c4b8a7dac4a681ba1a4dea6079ecf
marmellad <levits00@rambler.ru>
2019-06-11 22:15:41 +0300

    Updates

---
+++ precache-manifest.74b145c8cbcdceebf24b3ba0c00c40f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/supreme-disco/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "9f867f3297d062c3ed11",
+    "url": "/supreme-disco/static/js/runtime~main.9f867f32.js"
+  },
+  {
+    "revision": "f5aa939567d049142448",
+    "url": "/supreme-disco/static/js/main.f5aa9395.chunk.js"
+  },
+  {
+    "revision": "51c7823145d92a57dfda",
+    "url": "/supreme-disco/static/js/2.51c78231.chunk.js"
+  },
+  {
+    "revision": "f5aa939567d049142448",
+    "url": "/supreme-disco/static/css/main.4ef336e5.chunk.css"
+  },
+  {
+    "revision": "51c7823145d92a57dfda",
+    "url": "/supreme-disco/static/css/2.3e73ef47.chunk.css"
+  },
+  {
+    "revision": "6b9c933ecaf034098168a905a06a55e9",
+    "url": "/supreme-disco/index.html"
+  }
+];
